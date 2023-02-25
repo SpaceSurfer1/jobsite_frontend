b5592326a022655e6374102b502cdb3ed07a3b74
SpaceSurfer1 <99202999+SpaceSurfer1@users.noreply.github.com>
2023-02-25 22:36:37 +0530

    changes

--- src/CenterBox/MiddleBox/MiddleBox.js
+++ src/CenterBox/MiddleBox/MiddleBox.js
@@ -4,10 +4,19 @@ function MiddleBox() {
     const [text, setText] = useState("bhupesh");
     return(
         <div className="middlebox">
-            {text}
-            <button onClick={()=>setText("Mishra")}>
+            <h5>PostBox</h5>
+            {/* <button onClick={()=>setText("Mishra")}>
                 Click me!
-            </button>
+            </button> */}
+            <div class="postboxdiv">
+                <form>
+                    <textarea class="post-field"rows={7}cols={50}placeholder="Type...">
+
+                    </textarea>
+                    <br/>
+                    <input type="submit" value="Post"/>
+                </form>
+            </div>
         </div>
     );
     
