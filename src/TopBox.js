9694edba8a9ff9eb7d78237942e5c2d0cb191f3d
SpaceSurfer1 <99202999+SpaceSurfer1@users.noreply.github.com>
2023-02-25 18:11:08 +0530

    changes

--- src/TopBox.js
+++ src/TopBox.js
@@ -1,11 +1,13 @@
 import SearchBox from './SearchBox';
 import SiteNameBox from './SiteNameBox';
 import './TopBox.css';
+import TopOptionsBox from './TopOptions';
 function TopBox() {
     return(
         <div className="topbox">
             <SiteNameBox></SiteNameBox>
             <SearchBox></SearchBox>
+            <TopOptionsBox></TopOptionsBox>
         </div>
     );
     
